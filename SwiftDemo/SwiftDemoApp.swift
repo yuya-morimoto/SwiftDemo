889c2ef9b232a2257818ce427e3f999fcfee0460
developeeeer <gyu2243dev@gmail.com>
2023-03-19 23:32:26 +0900

    Initial Commit

---
+++ SwiftDemo/SwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemoApp.swift
+//  SwiftDemo
+//
+//  Created by Yuya Morimoto on R 5/03/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
