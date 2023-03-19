ee94f06f0906e787e9590beccfb1de8b1b6ef8e6
developeeeer <gyu2243dev@gmail.com>
2023-03-19 23:46:55 +0900

    update: font-size

--- SwiftDemo/ContentView.swift
+++ SwiftDemo/ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, SwiftUI!")
+            Text("Turtle Rock")
+                .font(.title)
+                
         }
         .padding()
     }
